74042a78a57cee52f0b082843326d7b94826215d
ci-mustafa akbari <akbarimustafa64@gmail.com>
2024-09-05 15:33:06 +0000

    feat: add ability to add new task to task table

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+    let datePicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datePicker, {format:"dd mmmm, yyyy", i18n: {done:"Select"}});
+    let category = document.querySelectorAll('select');
+    M.FormSelect.init(category);
   });
+
+  
